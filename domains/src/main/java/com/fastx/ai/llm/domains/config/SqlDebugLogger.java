ccde109d604ffbc646c83b1c1726f8e82298338a
C-L-STARK <x.stark.dylan@gmail.com>
2024-12-22 20:27:33 +0800

    update logger config and issue to dubbo.

--- domains/src/main/java/com/fastx/ai/llm/domains/config/SqlDebugLogger.java
+++ domains/src/main/java/com/fastx/ai/llm/domains/config/SqlDebugLogger.java
@@ -1,6 +1,7 @@
 package com.fastx.ai.llm.domains.config;
 
 import com.p6spy.engine.spy.appender.StdoutLogger;
+import org.slf4j.MDC;
 
 /**
  * @author stark
@@ -9,7 +10,7 @@ public class SqlDebugLogger extends StdoutLogger {
 
     @Override
     public void logText(String text) {
-        System.err.println(text);
+        System.err.println(MDC.get("traceId") + " " + text);
     }
 
 }
--- web/src/main/java/com/fastx/ai/llm/web/config/LoginInterceptor.java
+++ web/src/main/java/com/fastx/ai/llm/web/config/LoginInterceptor.java
@@ -2,10 +2,13 @@ package com.fastx.ai.llm.web.config;
 
 import cn.dev33.satoken.stp.StpUtil;
 import com.alibaba.fastjson2.JSON;
+import com.fastx.ai.llm.common.SnowflakeIdGenerator;
 import com.fastx.ai.llm.web.controller.entity.Response;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -17,9 +20,17 @@ import java.util.Objects;
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
 
+    @Autowired
+    SnowflakeIdGenerator snowflakeIdGenerator;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String satoken = request.getHeader("satoken");
+
+        String traceId = String.valueOf(snowflakeIdGenerator.generateId());
+        MDC.put("trace-id", traceId);
+        MDC.put("span-id", "web");
+        response.setHeader("X-Trace-Id", traceId);
 
         if (StringUtils.equals(request.getMethod(), "OPTIONS")) {
             response.setStatus(HttpServletResponse.SC_OK);
